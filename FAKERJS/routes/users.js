5558135b6ee65d158ccee0d272f898a26c5115da
Poliseno Riccardo <113579593+PolisenoRiccardo@users.noreply.github.com>
2025-03-10 18:56:33 +0000

    completati requisiti

--- FAKERJS/routes/users.js
+++ FAKERJS/routes/users.js
@@ -2,13 +2,6 @@ var express = require('express');
 var router = express.Router();
 var faker = require('faker'); 
 
-allpoets = fakePersonArray(10);
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(allpoets);
-});
-
 function createFakePerson()
 {
   let randomName = faker.name.findName(); // Rowan Nikolaus
@@ -27,8 +20,38 @@ function fakePersonArray(arrayLenght)
   let array = [];
   for (let i = 0; i < arrayLenght; i++) {
     array.push(createFakePerson());
+    console.log(array[i]);
   }
   return array;
 }
+
+allpoets = fakePersonArray(10);
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  console.log("visitatot");
+  res.render('index', {
+  title: 'Users',
+  poets: allpoets //Passa il vettore alla pagina index.pug
+  });
+});
+
+router.get('/:poet_username', (req, res) => { 
+  const poetUsername = req.params.poet_username;
+  for (i = 0; i < allpoets.length; i++) {
+    if (allpoets[i].card.username == poetUsername) {
+      var poet = allpoets[i];
+    } }
+  if (poet != null) {
+    res.render('poet', {
+      title: poet.name,
+      poet: poet
+    });
+  } else {
+    res.status(404).send('poet not found');
+  }
+
+});
+
 
 module.exports = router;
